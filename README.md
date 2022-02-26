37f5d80052a02a03f92c2783b98d7dbe70699514
Eric Moore <emoore.tech@gmail.com>
2022-02-26 14:38:11 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+https://sleepy-reaches-29007.herokuapp.com/
+
+https://git.heroku.com/sleepy-reaches-29007.git
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jfc11%a4806w1d8w+0(r$zpxl=te^xp*fnzz&0hc*2ddtgn85l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
